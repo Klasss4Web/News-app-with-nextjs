ffe6b72c7730de8bf5519b9fde2060ea7d12a801
Emmanuel Ochade <eochade16@gmail.com>
2022-11-27 13:35:03 +0100

    removed all unused variables and imports

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import { Footer } from '../components/Footer'
-import { Navbar } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
